b162a59472b43e41fc7c94da0b9133d4876268e2
daniel <daniel>
2025-07-27 11:23:48 +0100

    Revert "_"
    This reverts commit abe1f43aaaf29d17a44e973dff8b3fa4fc530a81.

--- json/blog.json.js
+++
@@ -1,12 +0,0 @@
-const cardsHtml = `<div class='card mb-3' data-all='card' data-unique='job' style='border-color:#006fcd'>
-		<div class='card-header d-flex flex-row justify-content-between align-items-center'>
-		Introduction and Colorifix Record
-		</div>
-		<div class='card-body'>
-			<p class='card-text'>Whilst undertaking analyst engagements we've been getting quite familiar with the industry, how it's shaping up and some of the key players and we think it's worth sharing some of the those insights through a database and website for those interested in the industry and how the landscape of interconnected companies is emerging.<br/><br/>At this point in the development of the industry it seems quite unclear, to us at least, how it may mature over time. Understanding how this industry may adopt and tweak comparative business models such as app store economics, cloud offerings and foundry services or how it may overcome it's unique challenge through innovation and collaboration is definitely a story we look forward to understanding and sharing.<br/><br/>Although fundamentally this is a database listing companies, recording the products, services and connections in doing so will inevitably outline the technology, science and computing that are being applied to facilitate the progression and adoption of the industry.</p>
-		</div>
-		<div class='card-footer text-muted d-flex flex-row  align-items-center'>
-      		<span style='margin-right:auto'>July 20th 2025</span>
-      		<span class='tagSpan' style='background-color:#006fcd; margin-left:3px'>SynbioCareers.com</span>
-    	</div>
-	</div>`;
