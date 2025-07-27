35f3042e1b8bcd3961c0ff23b7776a786ae439ea
daniel <daniel>
2025-07-27 12:27:26 +0100

    _

---
+++ json/json.js
@@ -0,0 +1,6 @@
+const statsTable = {"country":[["UK",1]],"industry":[["Plastics",1]]}
+const firmsTable = [{"name":"Colorifix","country":"UK","city":"Cambridge","description":"Colorifix Description","url":"colorifix.com","tags":["Plastic"],"products":[{"id":1,"firmId":1,"name":"Plastic","toBusiness":true,"atMarket":true,"trl":1,"deliverable":"Enzyme","industry":"Plastics","vehicle":"Enzyme","detail":"Enzyme machine"}],"industries":["Plastics"],"noOfProducts":1,"trl":"Level 1: Basic principles of concept are observed and reported"}]
+const speciesTable = [{"id":1,"name":"Bacteria1","organism":"Bacteria"},{"id":2,"name":"Virus1","organism":"Virus"},{"id":3,"name":"Fungi1","organism":"Fungi"},{"id":4,"name":"Enzyme1","organism":"Enzyme"}]
+const industriesTable = ["Plastics"]
+const tagsTable = ["Plastic"]
+const vectorsTable = ["Bacteria","Enzyme","Fungi","Virus"]
